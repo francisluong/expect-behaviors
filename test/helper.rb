9e0ffd573da1eb869d887399f2f5d48c91d832c3
Francis Luong (Franco) <fluong@salesforce.com>
2015-01-19 15:40:57 -0500

    a start

---
+++ test/helper.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+# gem 'minitest'
+# require 'minitest/autorun'
+# gem 'test-unit'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'expect/behaviors'
+
+# class Test::Unit::TestCase
+# end
---
+++ test/test_expect_behaviors.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+class TestExpectBehaviors < Test::Unit::TestCase
+  context "get things going" do
+    should "assert true" do
+      assert(true)
+    end
+
+    should "assert false" do
+      assert_false(false)
+    end
+  end
+
+end
