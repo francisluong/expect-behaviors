41cd146063d9225379e5bc9d7dbaee0f3b4b4d7d
Francis Luong (Franco) <fluong@salesforce.com>
2015-01-19 20:58:50 -0500

    tests for Expect::Match

--- lib/expect/match.rb
+++ lib/expect/match.rb
@@ -1,9 +1,37 @@
 module Expect
   class Match
-    attr_reader :buffer
+    attr_reader :buffer, :success
     def initialize(expression, buffer)
       @expression = expression
       @buffer = buffer
+      @matches = @buffer.match(@expression)
     end
+
+    def exact_match_string
+      @matches.nil? ? nil : @matches[0]
+    end
+
+    def substring_up_to_match
+      if @matches.nil?
+        nil
+      else
+        new_expr = substring_expression
+        @buffer.match(new_expr)[0]
+      end
+    end
+    alias_method :to_s, :substring_up_to_match
+
+    def substring_remainder
+      start_index = substring_up_to_match.length
+      @buffer[start_index..-1]
+    end
+
+    ###
+    private
+
+    def substring_expression
+      Regexp.new(".*?#{@expression.source}", @expression.options | Regexp::MULTILINE)
+    end
+
   end
 end
--- test/test_expect_match.rb
+++ test/test_expect_match.rb
@@ -2,12 +2,33 @@ require 'helper'
 require 'expect/match'
 
 class TestExpectMatch < Test::Unit::TestCase
-  context "placeholder" do
-    should "be able to get full buffer by calling #buffer method" do
-      expr = /bob/
-      buffer = "testing\nbob is cool"
-      match = Expect::Match.new(expr, buffer)
-      assert(buffer, match.buffer)
+  context "default" do
+    should "return full buffer by calling #buffer method" do
+      match = Expect::Match.new(expr1, buffer1)
+      assert(buffer1, match.buffer)
     end
+
+    should "return matching buffer text when calling #exact_match_string" do
+      match = Expect::Match.new(expr1, buffer1)
+      assert_equal(expr1.source, match.exact_match_string)
+    end
+
+    should "return the part of the buffer up to the first matching text when calling #substring_up_to_match" do
+      match = Expect::Match.new(expr1, buffer1)
+      assert_equal("testing\nbob", match.substring_up_to_match)
+    end
+
+    should "return the part of the buffer following the first matching text when calling #substring_remainder" do
+      match = Expect::Match.new(expr1, buffer1)
+      assert_equal(" is cool\nbob is good", match.substring_remainder)
+    end
+
+  end
+  def expr1
+    /bob/
+  end
+
+  def buffer1
+    "testing\nbob is cool\nbob is good"
   end
 end
